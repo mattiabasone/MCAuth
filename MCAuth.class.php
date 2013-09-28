c632188cf9c28f5174ffb67755c061f57a62b271
mattiabasone <mattia.basone@gmail.com>
2013-09-29 01:43:08 +0200

    Update MCAuth.class.php

--- MCAuth.class.php
+++ MCAuth.class.php
@@ -13,6 +13,23 @@ class MCAuth
     public $account = array();
     private $autherr;
 
+    // Generic function for cURL requests
+    private function curl_request($address) {
+        $request = curl_init();
+        curl_setopt($request, CURLOPT_HEADER, 0);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($request, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($request, CURLOPT_URL, $address);
+        return curl_exec($request);
+        curl_close($request);
+    }
+    
+    // Check if username is premium
+    public function check_premium($username) {
+        return $this->curl_request('https://www.minecraft.net/haspaid.jsp?user='.$username);
+    }
+
+    // Authentication function
     public function authenticate($username, $password) {
         // json array for POST authentication
         $json = array();
