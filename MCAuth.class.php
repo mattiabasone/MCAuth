9d37bdef3231dc15f3bdffc0a6d6e4042641c87c
mattiabasone <mattia.basone@gmail.com>
2013-09-29 02:28:25 +0200

    Update MCAuth class with better error reporting

--- MCAuth.class.php
+++ MCAuth.class.php
@@ -7,12 +7,12 @@
  * version: 1.0
  * info/support: mattia.basone@gmail.com
  */
-class MCAuth
-{
+class MCAuth {
 
     public $account = array();
-    private $autherr;
+    public $autherr;
 
+    
     // Generic function for cURL requests
     private function curl_request($address) {
         $request = curl_init();
@@ -28,8 +28,7 @@ class MCAuth
     public function check_premium($username) {
         return $this->curl_request('https://www.minecraft.net/haspaid.jsp?user='.$username);
     }
-
-    // Authentication function
+    
     public function authenticate($username, $password) {
         // json array for POST authentication
         $json = array();
@@ -44,16 +43,33 @@ class MCAuth
         curl_setopt($request, CURLOPT_POSTFIELDS, json_encode($json));
         curl_setopt($request, CURLOPT_URL, 'https://authserver.mojang.com/authenticate');
         $response = curl_exec($request);
+        $req_info = curl_getinfo($request);
         curl_close($request);
         $rjson = json_decode($response);
-       if (!isset($rjson->error)) {
-           $this->account['id'] = $rjson->selectedProfile->id;
-           $this->account['username'] = $rjson->selectedProfile->name;
-           $this->account['token'] = $rjson->accessToken;
-           return TRUE;
-       } else {
-           $this->autherr = $rjson->errorMessage;
-           return FALSE;
-       }
+        if ($req_info['http_code'] == '200') {
+            if (!isset($rjson->error) AND isset($rjson->selectedProfile->name)) {
+                $this->account['id'] = $rjson->selectedProfile->id;
+                $this->account['username'] = $rjson->selectedProfile->name;
+                $this->account['token'] = $rjson->accessToken;
+                $this->autherr = 'OK';
+                return TRUE;
+            } else {
+                $this->autherr = $rjson->errorMessage;
+                return FALSE;
+            }
+        } else {
+            if (isset($req_info['http_code'])) {
+                if (isset($rjson->error)) {
+                    $this->autherr = $rjson->errorMessage;
+                    return FALSE;
+                } else {
+                    $this->autherr = 'Server returned http code '.$req_info['http_code'];
+                    return FALSE;
+                }
+            } else {
+                $this->autherr = 'Server unreacheable';
+                return FALSE;
+            }
+        }
     }
 }
